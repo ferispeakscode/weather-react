bd70aab2147ca809ac408a75e39201502f5a2736
Feri <ferispeakscode@gmail.com>
2021-03-31 12:55:05 -0400

    Installed axios

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
 export default function Weather() {
+
+    function handleResponse(response) {
+        alert(`The weather in New York is ${response.data.main.temp}°C`);
+    }
+
+    let apiKey = "28dc556bb211b420ef0f1e534d06a1db";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+    axios.get(url).then(handleResponse);
+
     return (<h1>Hello React!</h1>);
 }
