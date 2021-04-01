5176d2e5f18d0c44f967268d424ce7798bc580a5
Feri <ferispeakscode@gmail.com>
2021-04-01 00:55:35 -0400

    Get weather statistics from API using axios

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 export default function Weather() {
     const [loaded, setLoaded] = useState(false);
     const [city, setCity] = useState();
+    const [weather, setWeather] = useState({});
 
     let form = (
         <form onSubmit={handleSubmit}>
@@ -18,13 +19,25 @@ export default function Weather() {
 
     function handleSubmit(event) {
         event.preventDefault();
-        console.log(`City is ${city}`);
-        setLoaded(true);
+
+        let apiKey = "81f516c244f3dd725e577ba1e814dedc";
+        let url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric&q=${city}`;
+
+        axios.get(url).then(handleResponse);
     }
 
-    // function handleResponse(response) {
+    function handleResponse(response) {
     //     alert(`The weather in New York is ${response.data.main.temp}°C`);
-    // }
+        setCity(response.data.name);
+        setWeather({ 
+            temperature: response.data.main.temp,
+            description: response.data.weather[0].description,
+            humidity: response.data.main.humidity,
+            wind: response.data.wind.speed,
+            icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+        });
+        setLoaded(true);
+    }
 
     // let apiKey = "28dc556bb211b420ef0f1e534d06a1db";
     // let url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric&q=New York`;
