38153d0d6078ceae77bd1f80b4fa9e3fe1fa4a5c
Feri <ferispeakscode@gmail.com>
2021-04-01 00:21:32 -0400

    Set up conditional rendering

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 export default function Weather() {
+    const [loaded, setLoaded] = useState(false);
 
     function handleResponse(response) {
         // alert(`The weather in New York is ${response.data.main.temp}°C`);
     }
 
     let apiKey = "28dc556bb211b420ef0f1e534d06a1db";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric&q=New York`;
 
     axios.get(url).then(handleResponse);
 
-    return (<h2>Hello React!</h2>);
+    if (loaded) {
+        return (
+            <div>
+                <h2>At least it is in CITY</h2>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h2>At least it is somewhere in the world...</h2>
+            </div>
+        );
+    }
 }
