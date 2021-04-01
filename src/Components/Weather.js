689581716cc561b0a733b1d68e889cf501959a04
Feri <ferispeakscode@gmail.com>
2021-04-01 00:31:09 -0400

    Handle city update and form submission

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -3,27 +3,38 @@ import axios from 'axios';
 
 export default function Weather() {
     const [loaded, setLoaded] = useState(false);
+    const [city, setCity] = useState();
 
     let form = (
-        <form>
-            <input type="search" placeholder="Enter a city..." />
+        <form onSubmit={handleSubmit}>
+            <input type="search" placeholder="Enter a city..." onChange={updateCity} />
             <input type="submit" value="Search" />
         </form>
     );
 
-    function handleResponse(response) {
-        // alert(`The weather in New York is ${response.data.main.temp}°C`);
+    function updateCity(event) {
+        setCity(event.target.value);
     }
 
-    let apiKey = "28dc556bb211b420ef0f1e534d06a1db";
-    let url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric&q=New York`;
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(`City is ${city}`);
+        setLoaded(true);
+    }
 
-    axios.get(url).then(handleResponse);
+    // function handleResponse(response) {
+    //     alert(`The weather in New York is ${response.data.main.temp}°C`);
+    // }
+
+    // let apiKey = "28dc556bb211b420ef0f1e534d06a1db";
+    // let url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=metric&q=New York`;
+
+    // axios.get(url).then(handleResponse);
 
     if (loaded) {
         return (
             <div>
-                <h2>At least it is in CITY</h2>
+                <h2>At least it is in {city}!</h2>
                 {form}
             </div>
         );
