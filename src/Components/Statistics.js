6297e2197c7e0aeea15de3f378e7764c13fa6dad
Feri <ferispeakscode@gmail.com>
2021-04-01 23:55:48 -0400

    Show all statistics in Statistics component

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -6,6 +6,12 @@ export default function Statistics(props) {
     return (
         <div>
             <h3>{props.city}</h3>
+            <img src={props.stats.icon} alt="weather icon" />
+            {props.stats.temperature}° C
+            {props.stats.description}
+            Feels like: {props.stats.feel}° C
+            Humidity: {props.stats.humidity}%
+            Wind: {props.stats.wind} m/s
         </div>
     );
 }
