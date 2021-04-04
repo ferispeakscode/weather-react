d8b45e0aa576ba122dfa6cf13067789084fb2ebd
Feri <ferispeakscode@gmail.com>
2021-04-04 00:36:54 -0400

    Added format date/time function to LastUpdated component for proper display

--- src/Components/LastUpdated.js
+++ src/Components/LastUpdated.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 export default function LastUpdated(props) {
-    
-    
-    return(<h1>{props.date}</h1>);
+    let lastUpdated = formatDateTime(props.date);
+
+    function formatDateTime(timestamp) {
+        let date = new Date(timestamp);
+        let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        let day = "";
+        let hour = date.getHours();
+        let minutes = date.getMinutes();
+        let meridiem = "AM";
+
+        day = daysOfWeek[date.getDay()];
+
+        if (hour > 12) {
+            meridiem = "PM";
+            hour -= 12;
+        }
+
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+        }
+
+        return `${day}, ${hour}:${minutes} ${meridiem}`;
+    }
+
+    return (<h4>{lastUpdated}</h4>);
 }
