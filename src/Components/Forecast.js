d7b583e37d66d37ec4fee20baad821aaa992e955
Feri <ferispeakscode@gmail.com>
2021-04-04 22:25:50 -0400

    Created basic Forecast component structure

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -2,6 +2,23 @@ import React from 'react';
 
 export default function Forecast(props) {
     console.log(props.forecast);
-    
-    return(<div className="container"></div>);
+    if (props.forecast) {
+        console.log(props.forecast[0].dt);
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        
+                    </div>
+                </div>
+            </div>
+        );
+    } else {
+        return (
+            <div className="container">
+                <div className="row">
+                </div>
+            </div>
+        );
+    }
 }
