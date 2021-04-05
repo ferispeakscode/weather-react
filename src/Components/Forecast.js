672786120b63b55dbf66547803d5d27dd5a9fabb
Feri <ferispeakscode@gmail.com>
2021-04-04 22:29:59 -0400

    Updated Forecast with ForecastItem(s)

--- src/Components/Forecast.js
+++ src/Components/Forecast.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ForecastItem from './ForecastItem';
 
 export default function Forecast(props) {
     console.log(props.forecast);
@@ -8,7 +9,22 @@ export default function Forecast(props) {
             <div className="container">
                 <div className="row">
                     <div className="col">
-                        
+                        <ForecastItem time={props.forecast[0].dt} icon={0} max={0} min={0} />
+                    </div>
+                    <div className="col">
+                        <ForecastItem time={props.forecast[0].dt} icon={0} max={0} min={0} />
+                    </div>
+                    <div className="col">
+                        <ForecastItem time={props.forecast[0].dt} icon={0} max={0} min={0} />
+                    </div>
+                    <div className="col">
+                        <ForecastItem time={props.forecast[0].dt} icon={0} max={0} min={0} />
+                    </div>
+                    <div className="col">
+                        <ForecastItem time={props.forecast[0].dt} icon={0} max={0} min={0} />
+                    </div>
+                    <div className="col">
+                        <ForecastItem time={props.forecast[0].dt} icon={0} max={0} min={0} />
                     </div>
                 </div>
             </div>
