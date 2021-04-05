80a1f51123bf378ac272fe896ca4ba83f0a65479
Feri <ferispeakscode@gmail.com>
2021-04-04 22:56:45 -0400

    Corrected ForecastItem property display

--- src/Components/ForecastItem.js
+++ src/Components/ForecastItem.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 export default function ForecastItem(props) {
+    let image = `http://openweathermap.org/img/wn/${props.icon}.png`;
+
     return(
         <div className="col">
-            {props.time}
-            {props.icon}
-            {props.max}
-            {props.min}
+            <h6>{props.time}</h6>
+            <img src={image} alt={props.description} />
+            <h6><strong>{props.max}°</strong></h6>
+            <h6>{props.min}°</h6>
         </div>
     );
 }
