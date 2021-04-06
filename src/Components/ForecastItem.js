ca3636857b7518905bd622d367aa149ccbfa715d
Feri <ferispeakscode@gmail.com>
2021-04-05 23:01:30 -0400

    Use DateItem to show forecast times

--- src/Components/ForecastItem.js
+++ src/Components/ForecastItem.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import DateItem from './DateItem';
 
 export default function ForecastItem(props) {
     let image = `http://openweathermap.org/img/wn/${props.icon}.png`;
 
     return(
         <div className="col">
-            <h6>{props.time}</h6>
+            <DateItem timestamp={props.time} condensed={true} />
             <img src={image} alt={props.desc} />
             <h6><strong>{props.max}°</strong></h6>
             <h6>{props.min}°</h6>
