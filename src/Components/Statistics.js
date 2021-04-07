6662130bffb781f0ed8792c53f03fa582f415d5a
Feri <ferispeakscode@gmail.com>
2021-04-06 23:30:40 -0400

    Updated classes for Statistics component

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Statistics(props) {
 
     return (
-        <div className="container">
+        <div className="container current-container">
             <div className="row justify-content-center">
                 <div className="col-12 col-md-4 my-1 text-center">
                     <h3>{props.city}</h3>
-                    <h3><img src={props.stats.icon} alt="weather icon" />
+                    <h3><img src={props.stats.icon} class="current-weather-icon" alt="weather icon" />
                     {props.stats.temperature}° C</h3>
                     <h4 id="description">{props.stats.description}</h4>
                 </div>
