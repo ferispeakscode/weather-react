944f4aece6eb26f2ff8dbdc3fa542e04008035d7
Feri <ferispeakscode@gmail.com>
2021-04-18 19:38:27 -0400

    Adjusted line height of extra statistics (right column)

--- src/Components/Statistics.js
+++ src/Components/Statistics.js
@@ -11,7 +11,7 @@ export default function Statistics(props) {
                     {props.stats.temperature}° {props.tempUnit}</h3>
                     <h4 id="description">{props.stats.description}</h4>
                 </div>
-                <div className="col-12 col-md-4 mt-4">
+                <div className="col-12 col-md-4 mt-3" id="extraStats">
                     <h4>Feels like: {props.stats.feel}° {props.tempUnit}</h4>
                     <h4>Humidity: {props.stats.humidity}%</h4>
                     <h4>Wind: {props.stats.wind} {props.windUnit}</h4>
